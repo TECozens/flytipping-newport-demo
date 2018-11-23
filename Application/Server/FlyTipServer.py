e5927cc2ec79af54583718e45b0e687f0a49dee5
Tomas <cozenste@cardiff.ac.uk>
2018-11-23 12:32:21 +0000

    Implementing Server into Official Site

--- Application/Server/FlyTipServer.py
+++ Application/Server/FlyTipServer.py
@@ -2,16 +2,16 @@ import os
 from flask import Flask, redirect, request, render_template, make_response, escape, session
 import sqlite3
 
-DATABASE = 'databases/MainDatabase.db'
+DATABASE = 'Site/Resources/Database/MainDatabase.db'
 
 app = Flask(__name__)
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 
-@app.route("/")
+@app.route("/flyReport")
 def open_main_page():
-    return render_template('index.html')
+    return render_template('flyForm1.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
