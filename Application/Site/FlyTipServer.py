99567978469211ae8ff4bdbb176ada8ebe7e8484
Tomas Cozens <CozensTE@cardiff.ac.uk>
2018-11-25 20:30:14 +0000

    Adding form Fields

--- Application/Site/FlyTipServer.py
+++ Application/Site/FlyTipServer.py
@@ -11,7 +11,7 @@ ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 @app.route("/flyReport")
 def open_main_page():
-    return render_template('ReportForm.html')
+    return render_template('ReportForm1.html')
 
 @app.route("/admin")
 def open_admin_page():
