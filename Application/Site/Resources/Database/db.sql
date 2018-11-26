4e57141b55233e11c53b05f7b132501b254a890f
Saumik Dhar <Dhars@cardiff.ac.uk>
2018-11-26 11:16:19 +0000

    updated server

--- Application/Site/Resources/Database/db.sql
+++ Application/Site/Resources/Database/db.sql
@@ -1,7 +1,11 @@
-  CREATE TABLE `Reports` ( 
-    `ID` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    `tipLocation` TEXT NOT NULL,
-    `locationDescription` TEXT NOT NULL,
-    `wasteID` INTEGER NOT NULL,
-    `wasteSize` TEXT NOT NULL,
-    `pathID` TEXT NOT NULL )
+CREATE TABLE "Reports" ( 
+  `ID` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  `tipLocation` TEXT NOT NULL,
+  `locationDescription` TEXT NOT NULL,
+  `wasteID` INTEGER NOT NULL,
+  `wasteSizeID` INTEGER NOT NULL,
+  `wasteDescription` TEXT NOT NULL,
+  `imageID` TEXT NOT NULL,
+  `witness` TEXT NOT NULL,
+  `witnessID` INTEGER NOT NULL,
+  `emailaddress` TEXT NOT NULL )
